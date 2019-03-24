048cd7c66239672845b6ed82ba951b7a935c75df
Gary Henderson <hendergy@gmail.com>
2019-03-24 16:19:56 -0400

    connected app to Firebase DB

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,7 +1,21 @@
-export const createProject = () => {
+export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
-
-    dispatch({ type: "CREATE_PROJECT", project: "project" });
+    const firestore = getFirestore();
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "Gary",
+        authorLastName: "Henderson",
+        authorId: 12345,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project: "project" });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_PROJECT_ERROR", err });
+      });
   };
 };
