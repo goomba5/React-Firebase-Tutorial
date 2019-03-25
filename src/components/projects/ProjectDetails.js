34a61239e250e9c4aae81e70a65e5d80c022d7be
Gary Henderson <hendergy@gmail.com>
2019-03-25 18:17:42 -0400

    set up SignIn auth with Firebase DB

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,21 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const ProjectDetails = props => {
-  const id = props.match.params.id;
-  return (
-    <div className="container section project-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <div className="card-title">Project Title - {id}</div>
-          <p>Duh stuff</p>
-        </div>
-        <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by Dredgen Yor</div>
-          <div>March 21, 2018</div>
+  const { project } = props;
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <div className="card-title">{project.title}</div>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>
+              {project.authorFirstName} {project.authorLastName}
+            </div>
+            <div>March 21, 2018</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading project...</p>
+      </div>
+    );
+  }
 };
 
-export default ProjectDetails;
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.projects;
+  const project = projects ? projects[id] : null;
+  return {
+    project: project
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }])
+)(ProjectDetails);
