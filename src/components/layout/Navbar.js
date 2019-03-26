76576184d1ffdcd1f22685d3700c4e381ebd79f0
Gary Henderson <hendergy@gmail.com>
2019-03-26 00:15:12 -0400

    upated Navbar to show links based on log in/out

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,23 +5,23 @@ import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
 const Navbar = props => {
-  // const { auth } = props;
+  const { auth } = props;
   // console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           Tutorial App
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = state => {
-  console.log(state);
+  // console.log(state);
   return {
     auth: state.firebase.auth
   };
