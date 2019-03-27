21d5aeab1982e8dc0a3d9ba7b14d243054461e6c
Gary Henderson <hendergy@gmail.com>
2019-03-27 16:08:05 -0400

    created redirect from Create Project to Dashboard

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,9 @@ const ProjectSummary = ({ project }) => {
     <div className="card z-depth-0 project summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Posted by Dredgen Yor</p>
+        <p>
+          Posted by {project.authorFirstName} {project.authorLastName}
+        </p>
         <p className="grey-text">March 21, 2018 at 4:11pm</p>
       </div>
     </div>
